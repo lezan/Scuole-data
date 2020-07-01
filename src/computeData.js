bf8b598114c4b1fc7a8e6c42803f3c1b702cee67
lezan <zanchileonardo@gmail.com>
2020-07-01 17:45:36 +0200

    Still problem with multi entry response and wrong result.

--- src/computeData.js
+++ src/computeData.js
@@ -12,37 +12,68 @@ module.exports = {
 	},
 	
 	getListAddress: (data) => {
-		const listAddress = data.map((d) => `${d.INDIRIZZOSCUOLA}, ${d.CAPSCUOLA}, ${d.DESCRIZIONECOMUNE}, ${d.PROVINCIA} Italy`);
+		// const listAddress = data.map((d) => `Italy, ${d.REGIONE}, ${d.PROVINCIA}, ${d.DESCRIZIONECOMUNE}, ${d.INDIRIZZOSCUOLA}`);
+		const listAddress = data.map((d) => `${d.INDIRIZZOSCUOLA}, ${d.DESCRIZIONECOMUNE}, ${d.PROVINCIA}`);
 	
 		return listAddress;
 	},
 
-	checkNumberResult: (data, type) => {
+	checkNumberResult: (data, type, listAddress) => {
+		let countMore = 0;
+		let countZero = 0;
+		let countOne = 0;
+
+		const listMore = {};
+
 		if (type === 'single') {
 			if (data.length > 1) {
 				console.log('Error. More entry from geocoding: ', data.length);
+				countMore++;
+			} else if (data.length < 1) {
+				console.log('Error. 0 entry from geocoding: ', data.length);
+				countZero++;
+			} else {
+				countOne++;
 			}
 		} else {
-			data.forEach((d) => {
+			data.forEach((d, index) => {
 				if (d.value.length > 1) {
-					console.log('Error. More entry from geocoding: ', d.value.length);
+					// console.log('Error. More entry from geocoding: ', d.value.length);
+					// console.log(listAddress[index]);
+					// console.log('-------------------');
+					listMore[`${listAddress[index]}_${index}`] = d.value;
+					countMore++;
+				} else if (data.length < 1) {
+					// console.log('Error. 0 entry from geocoding: ', data.length);
+					// console.log(listAddress[index]);
+					// console.log('-------------------');
+					countZero++;
+				} else {
+					countOne++;
 				}
 			});
 		}
+
+		// console.log(Object.keys(listMore).length);
+		fs.writeFileSync('./data/listMore.json', JSON.stringify(listMore));
+
+		console.log('More entry: ', countMore);
+		console.log('Zero entry: ', countZero);
+		console.log('One entry: ', countOne);
 	},
 
 	getLatLong: (data, type) => {
 		if (type === 'single') {
 			const result = data.map((d) => ({
-				lat: d.latitude,
-				long: d.longitude,
+				lat: d.latitude || 0,
+				long: d.longitude || 0,
 			}));
 
 			return result;
 		} else {
 			const result = data.map((d) => ({
-				lat: d.value[0].latitude,
-				long: d.value[0].longitude,
+				lat: d.value[0].latitude || 0,
+				long: d.value[0].longitude || 0,
 			}));
 
 			return result;
@@ -67,6 +98,14 @@ module.exports = {
 
 	saveListAddress: (listAddress) => {
 		fs.writeFileSync('./data/listAddress.json', JSON.stringify(listAddress));
+	},
+
+	saveGeocoderResponse: (response) => {
+		fs.writeFileSync('./data/geocoderResponse.json', JSON.stringify(response));
+	},
+
+	saveLatLong: (listLatLong) => {
+		fs.writeFileSync('./data/listLatLong.json', JSON.stringify(listLatLong));
 	},
 
 	saveData: async (data, filename) => {
--- src/index.js
+++ src/index.js
@@ -7,29 +7,35 @@ const main = async () => {
 	// console.log(data[0]);
 
 	// Recupera la lista degli indirizzi.
-	const listAddress = computeData.getListAddress(data);
+	const listAddress = computeData.getListAddress(data.slice(0, 1000));
 	// console.log(listAddress[0]);
 
+	// Salvo la lista degli indirizzi estratti.
+	computeData.saveListAddress(listAddress);
+
 	// Geocoding di un singolo indirizzo.
-	const geocoderResponse = await geocoder.singleGeocoding([listAddress[0]]);
+	// const geocoderResponse = await geocoder.singleGeocoding([listAddress[0]]);
 	// console.log(geocoderResponse);
 
 	// Geocoding batch degli indirizzi.
-	// const geocoderResponse = await geocoder.batchGeocoding([listAddress[0]]);
+	const geocoderResponse = await geocoder.batchGeocoding(listAddress);
+
+	// Salvo tutta la risposta del geocoder.
+	computeData.saveGeocoderResponse(geocoderResponse);
 
 	// Controllo se ad un indirizzo corrispondono più risultati restituiti dal geocoding.
-	computeData.checkNumberResult(geocoderResponse, 'single');
-	// computeData.checkNumberResult(geocoderResponse, 'batch');
+	// computeData.checkNumberResult(geocoderResponse, 'single', listAddress);
+	computeData.checkNumberResult(geocoderResponse, 'batch', listAddress);
 
-	const listLatLong = computeData.getLatLong(geocoderResponse, 'single');
-	// const listLatLong = computeData.getLatLong(geocoderResponse, 'batch');
+	// const listLatLong = computeData.getLatLong(geocoderResponse, 'single');
+	const listLatLong = computeData.getLatLong(geocoderResponse, 'batch');
 	// console.log(listLatLong[0]);
 	
 	// Salvo in locale la lista delle coordinate ottenuta. 
-	computeData.saveListAddress(listLatLong);
+	computeData.saveLatLong(listLatLong);
 
 	// Mergio i dati originali con le coordinate ottenute.
-	const mergedData = computeData.mergeData([data[0]], listLatLong);
+	const mergedData = computeData.mergeData(data.slice(0, 1000), listLatLong);
 	// console.log(mergedData);
 
 	// Salvo in locale i dati mergati.
