2c992e4ec60eade6997f08e1bf7eb2d7df8ff7c5
lezan <zanchileonardo@gmail.com>
2020-07-03 16:14:19 +0200

    New data merge with Aosta and so on.

--- src/getData.js
+++ src/getData.js
@@ -4,7 +4,7 @@ const d3Collection = require('d3-collection');
 
 module.exports = {
 	readData: async () => {
-		const importData = fs.readFileSync('./data/SCUANAGRAFESTAT20202120200901.csv');
+		const importData = fs.readFileSync('./data/allData.csv');
 		const data = await neatCsv(importData, { separator: ',' });
 	
 		return data;
--- src/runData.js
+++ src/runData.js
@@ -4,8 +4,13 @@ const computeData = require('./computeData.js');
 const main = async () => {
 	const data = await getData.readData();
 	const dataComune = getData.getDataByComune(data);
+	const dataProvincia = getData.getDataByProvincia(data);
+	const dataRegione = getData.getDataByRegione(data);
 
 	computeData.saveData(dataComune, 'dataComune');
+	computeData.saveData(dataProvincia, 'dataProvincia');
+	computeData.saveData(dataRegione, 'dataRegione');
+
 };
 
 main();
